ebe2e717bf2a84fcd28b154f66b4913976f02ce9
Leo Liang <leozc@leozc.com>
2016-08-22 01:38:43 -0700

    first cut of working example

--- lambda_streaming/index.js
+++ lambda_streaming/index.js
@@ -3,17 +3,16 @@ console.log('Loading function');
 
 exports.handler = (event, context, callback) => {
     //console.log('Received event:', JSON.stringify(event, null, 2));
-		var host="http://47.88.33.96:3000/channel/public"; //get default for now
+		// map to http://IP:port/channel/public
+		var host="http://47.88.79.24:3000/public"; //get default for now
     console.log(event);
 		var socket = require('socket.io-client')(host);
     event.Records.forEach((record) => {
         console.log(record.eventID);
         console.log(record.eventName);
         console.log('DynamoDB Record: %j', record.dynamodb);
-				require('socket.io-client');
 
-				var socket = require('socket.io-client')(host);
-				socket.emit("signal",JSON.stringify(record));
+				socket.emit("signal", JSON.stringify(record));
     });
 
     callback(null, `Successfully processed ${event.Records.length} records.`);
